957fd6e05cbd4654f7a6f2eec35eda53d32ede67
loinhanyeuthuong <huongtamky18@gmail.com>
2025-07-25 15:31:05 +0700

    Add files via upload

---
+++ admin/users/remember.php
@@ -0,0 +1,5 @@
+<?php
+
+ $_REMEMBER = array (
+  'admin' => '65dc844646f6fdcaae3eed15e5377be6',
+);
---
+++ admin/users/users.php
@@ -0,0 +1,10 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$nIl6YS6F$FPlSrwEhIN/qKmcIhPPHZ1',
+    'role' => 'superadmin',
+  ),
+);
